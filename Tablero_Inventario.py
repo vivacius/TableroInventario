454bf7174baf628fc9c811677500041852209a4f
vivacius <138029679+vivacius@users.noreply.github.com>
2025-06-03 23:47:21 -0500

    Update Tablero_Inventario.py

--- Tablero_Inventario.py
+++ Tablero_Inventario.py
@@ -10,7 +10,7 @@ import json
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 
 # Leer credenciales desde Streamlit Secrets
-creds_dict = json.loads(st.secrets["GOOGLE_CREDENTIALS"])
+creds_dict = st.secrets["GOOGLE_CREDENTIALS"]
 creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 client = gspread.authorize(creds)
 
